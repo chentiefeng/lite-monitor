f91dc29f79e9e4fde77df7974b56ebf3c05b0570
chentiefeng <chentiefeng@aliyun.com>
2020-04-17 15:00:51 +0800

    飞书BUG修复

--- src/main/java/me/ctf/lm/util/FeishuTalkHelper.java
+++ src/main/java/me/ctf/lm/util/FeishuTalkHelper.java
@@ -26,7 +26,7 @@ public class FeishuTalkHelper {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json;charset=UTF-8");
         HttpEntity<String> r = new HttpEntity<>("{\"title\":" + title + ",\"text\":" + content + "}", headers);
-        REST_TEMPLATE.postForObject("https://oapi.dingtalk.com/robot/send?access_token=" + token, r, String.class);
+        REST_TEMPLATE.postForObject("https://open.feishu.cn/open-apis/bot/hook/" + token, r, String.class);
     }
 
 }
